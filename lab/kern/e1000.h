6e29613f07cdbd3c91b79ed383faa5a83ddf377f
jjhlzn <jinjunhang@hotmail.com>
2014-07-18 18:02:04 +0800

    exercise 3

--- lab/kern/e1000.h
+++ lab/kern/e1000.h
@@ -1,4 +1,9 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#define PCI_VENDOR_E1000   0x8086
+#define PCI_PRODUCT_E1000  0x100e
+
+int e1000_attach(struct pci_func *pcif);
+
 #endif	// JOS_KERN_E1000_H
--- lab/kern/pci.c
+++ lab/kern/pci.c
@@ -30,6 +30,7 @@ struct pci_driver pci_attach_class[] = {
 
 // pci_attach_vendor matches the vendor ID and device ID of a PCI device
 struct pci_driver pci_attach_vendor[] = {
+	{ PCI_VENDOR_E1000, PCI_PRODUCT_E1000, &e1000_attach},
 	{ 0, 0, 0 },
 };
 
